41cf30e84a851a411758298233f6c387e424d113
HameemH <hameemhossain2@gmail.com>
2025-07-25 12:44:17 +0600

    db connected

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\DB;
+
+Route::get('/db', function () {
+    try {
+        DB::connection()->getPdo();
+        return " Database connected successfully.";
+    } catch (\Exception $e) {
+        return " Could not connect to the database. Error: " . $e->getMessage();
+    }
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
